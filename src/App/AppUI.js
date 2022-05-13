cb89fa05e7a380897e5d2c783c3069fc9b2b420b
Torcoroma <torcoromaarias@gmail.com>
2022-05-13 12:30:04 -0500

    Agrgados los efectos

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,6 +6,8 @@ import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
 function AppUI({
+  error,
+  loading,
   totalTodos,
   completedTodos,
   searchValue,
@@ -22,6 +24,9 @@ function AppUI({
         <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
         <TodoList>
+          {error && <p>Lo sentimos :( error</p>}
+          {loading && <p>Cargando......</p>}
+          {!loading && !searchedTodo.length && <p> ! Crea tu Primer todos!</p>}
           {searchedTodo.map((todo) => (
             <TodoItem
               key={todo.text}
--- src/App/index.js
+++ src/App/index.js
@@ -2,24 +2,63 @@ import React from "react";
 import "./App.css";
 import { AppUI } from "./AppUI";
 
-function App() {
-  // GUARDADO EN LOCALSTORY
-  const localStorageTodo = localStorage.getItem("TODOS_V1");
-  let persedTodos;
+function useLocalStorage(itemName, initialValue) {
+  const [error, seterror] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
 
-  if (!localStorageTodo) {
-    localStorage.setItem("TODOS_V1", JSON.stringify([]));
-    persedTodos = [];
-  } else {
-    persedTodos = JSON.parse(localStorageTodo);
-  }
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+        setItem(parsedItem);
+        setLoading(false);
+      } catch (error) {
+        seterror(error);
+      }
+    }, 1000);
+  });
+
+  // funcion que une localStore
+
+  const seveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      seterror(error);
+    }
+  };
+
+  return { item, seveItem, loading, error };
+}
+
+function App() {
+  const {
+    item: todos,
+    seveItem: seveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_V1", []);
+  // GUARDADO EN LOCALSTORY
+
   // REACIONA A TRUE LA LINEA MARCADO LEIDO
-  const [todos, setTodos] = React.useState(persedTodos);
+
   const [searchValue, setSearchValue] = React.useState("");
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
   // PARA BUCAR LA TAREA FILTRAR
   let searchedTodo = [];
+
   if (!searchValue.length >= 1) {
     searchedTodo = todos;
   } else {
@@ -31,11 +70,6 @@ function App() {
   }
 
   // FUNCION PARA ELIMINAR DEFINITIVAMENTE CON LOCAL STARY
-  const seveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem("TODOS_V1", stringifiedTodos);
-    setTodos(newTodos);
-  };
 
   // PARA MARCAR COMO LEIDO O HECHO
   const completeTodo = (text) => {
@@ -53,8 +87,11 @@ function App() {
     newTodos.splice(todoIndex, 1);
     seveTodos(newTodos);
   };
+
   return (
     <AppUI
+      loading={loading}
+      error={error}
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue}
